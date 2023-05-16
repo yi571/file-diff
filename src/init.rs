4c43d98c9ea403f9e76c65c48632e172f5f6cb70
yi571 <yi571@outlook.com>
2023-05-16 10:14:49 +0800

    feat: The progress bar is now displayed during the getting hash.

--- src/init.rs
+++ src/init.rs
@@ -5,6 +5,7 @@ use std::{
 };
 use time::OffsetDateTime;
 use walkdir::{DirEntry, WalkDir};
+use indicatif::{ProgressBar, ProgressStyle};
 
 use crate::hashing::{self, save_csv, CsvData};
 
@@ -21,30 +22,46 @@ fn check_init_folder_exist(path: &PathBuf) {
     let mut init_path: PathBuf = path.clone();
     init_path.push(".file-diff");
     if init_path.exists() {
-        println!("folder existed");
+        println!("record folder existed");
     } else {
         fs::create_dir(&init_path).unwrap();
         println!("folder created");
 
+        let spinner_style = ProgressStyle::with_template("{prefix:.bold.dim} {spinner} {wide_msg}")
+        .unwrap()
+        .tick_chars("⠁⠂⠄⡀⢀⠠⠐⠈ ");
+
         let mut data: Vec<CsvData> = Vec::new();
         let walker = WalkDir::new(&path).into_iter();
+        let total_count = walker.count();
+        let pb = ProgressBar::new(total_count.try_into().unwrap());
+        let walker = WalkDir::new(&path).into_iter();
+        pb.set_style(spinner_style.clone());
+        let mut count = 0;
         for file in walker
             .filter_entry(|e| !is_hidden(e))
             .filter_map(|e| e.ok())
         {
             if file.metadata().unwrap().is_file() {
                 let file_path: String = String::from(file.path().to_string_lossy());
-                println!("get hash from {} ", file_path);
+                // println!("get hash from {} ", file_path);
+                pb.set_style(spinner_style.clone());
+                // pb.set_prefix(format!("get hash from {} ", file_path));
+                pb.set_message(format!("get hash from {} ", file_path));
+                pb.inc(1);
                 let csv_data: CsvData = hashing::get_file_hash(file_path);
 
                 data.push(csv_data);
+                count+=1;
             }
+            
         }
-
+        
         let local = OffsetDateTime::now_local().unwrap();
 
-        init_path.push(format!("{}_init.csv", local.unix_timestamp()));
+        init_path.push(format!("{}_init", local.unix_timestamp()));
         save_csv(init_path, data);
+        pb.finish_with_message(format!("Done! Total File:{count}"));
     }
 }
 
--- src/record.rs
+++ src/record.rs
@@ -2,6 +2,7 @@ use std::{path::PathBuf, env};
 
 use time::OffsetDateTime;
 use walkdir::{DirEntry, WalkDir};
+use indicatif::{ProgressBar, ProgressStyle};
 
 use crate::hashing::{CsvData, self, save_csv};
 
@@ -16,24 +17,37 @@ pub fn record(msg:String, path: Option<PathBuf>){
     init_path.push(".file-diff");
 
     if init_path.exists(){
+        let spinner_style = ProgressStyle::with_template("{prefix:.bold.dim} {spinner} {wide_msg}")
+        .unwrap()
+        .tick_chars("⠁⠂⠄⡀⢀⠠⠐⠈ ");
         let mut data: Vec<CsvData> = Vec::new();
         let walker = WalkDir::new(&path).into_iter();
+        let total_count = walker.count();
+        let pb = ProgressBar::new(total_count.try_into().unwrap());
+        pb.set_style(spinner_style.clone());
+        let walker = WalkDir::new(&path).into_iter();
+        let mut count = 0;
         for file in walker
             .filter_entry(|e| !is_hidden(e))
             .filter_map(|e| e.ok())
         {
             if file.metadata().unwrap().is_file() {
                 let file_path: String = String::from(file.path().to_string_lossy());
-                println!("get hash from {} ", file_path);
+                // println!("get hash from {} ", file_path);
+                pb.set_style(spinner_style.clone());
+                pb.set_message(format!("get hash from {} ", file_path));
+                pb.inc(1);
                 let csv_data: CsvData = hashing::get_file_hash(file_path);
-
+                
                 data.push(csv_data);
+                count+=1;
             }
         }
+        
         let local = OffsetDateTime::now_local().unwrap();
-        init_path.push(format!("{}_{}.csv", local.unix_timestamp(), msg));
+        init_path.push(format!("{}_{}", local.unix_timestamp(), msg));
         save_csv(init_path, data);
-
+        pb.finish_with_message(format!("Done! Total File:{count}"));
     } else {
         println!("folder not exist");
     }
